b5a06cc4468edb9f6ebac0c8a25e7e50e1b2318c
PIPEFD <91981749+PIPEFD@users.noreply.github.com>
2023-07-11 17:17:55 +0000

    Continuamos

---
+++ rendu/first_word/first_word.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_word.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 16:24:22 by codespace         #+#    #+#             */
+/*   Updated: 2023/07/11 17:16:48 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+
+void first_word(char *string)
+{
+	int i;
+
+	i = 0;
+	while(string[i] == 32 || string[i] == 9)
+		i++;
+	while ((string[i] != 32 && string[i] != 9) && string[i])
+	{
+		write(1, &string[i], 1);
+		i++;
+	}
+}
+
+int main (int argc, char **argv)
+{
+	if (argc == 2)
+		first_word(argv[1]);
+	write (1, "\n", 1);
+	return (0);	
+}
