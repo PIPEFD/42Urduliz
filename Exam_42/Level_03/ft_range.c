7179338bd489ed279e1d93f97df3e4e385801aca
PIPEFD <91981749+PIPEFD@users.noreply.github.com>
2023-08-08 20:30:46 +0000

    push

--- Exam_42/Level_03/ft_range.c
+++ Exam_42/Level_03/ft_range.c
@@ -6,7 +6,33 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 18:36:59 by codespace         #+#    #+#             */
-/*   Updated: 2023/08/05 18:37:00 by codespace        ###   ########.fr       */
+/*   Updated: 2023/08/05 21:23:30 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+int *ft_range(int start, int end)
+{
+	int i;
+	int len;
+	int *result;
+
+	i = 0;
+	len = (end - start) + 1;
+	result = (int *)malloc(sizeof(int) * len );
+	while (i < len)
+	{
+		if (start < end)
+		{
+			result[i] = start;
+			start++;
+			i++;
+		}
+		else
+			result[i] = start;
+			start--;
+			i++;			
+	}
+	return (result);
+}
--- Exam_42/Level_03/ft_rrange.c
+++ Exam_42/Level_03/ft_rrange.c
@@ -1,12 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lcm.c                                              :+:      :+:    :+:   */
+/*   ft_rrange.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/05 21:06:39 by codespace         #+#    #+#             */
-/*   Updated: 2023/08/05 21:06:40 by codespace        ###   ########.fr       */
+/*   Created: 2023/08/07 12:22:09 by codespace         #+#    #+#             */
+/*   Updated: 2023/08/07 12:45:15 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+int *ft_rrange(int start, int end)
+{
+    int *range;
+    int i = 0;
+    int len = end -start + 1;
+    
+    if (start > end)
+        return(ft_rrange(end, start));
+    range = (int *)malloc(sizeof(int) * len);
+    if (range)
+    {
+        while(i < len)
+            {
+                range[i] = end;
+                end --;
+                i++;
+            }
+    }
+    return (range);
+}
