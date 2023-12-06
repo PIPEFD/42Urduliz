4b21dd0dafa16f6be604283fd62129e4760ef935
David Felipe Bonilla Ladino <dbonilla@c3r2s5.42urduliz.com>
2023-12-06 17:28:10 +0100

    BONUS

---
+++ Minitalk/src_bonus/server_bonus.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbonilla <dbonilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/28 20:20:06 by codespace         #+#    #+#             */
+/*   Updated: 2023/12/06 17:07:21 by dbonilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minitalk.h"
+
+void	ft_rsignal(int sig, siginfo_t *info, void *context)
+{
+	static int				i = 0;		
+	static unsigned char	c = 0;
+	static pid_t			pd = 0;
+
+	(void) context;
+	if (!pd)
+		pd = info->si_pid;
+	c |= (sig == SIGUSR2);
+	if (++i == 8)
+	{
+		i = 0;
+		if (!c)
+		{
+			kill(pd, SIGUSR2);
+			pd = 0;
+			return ;
+		}
+		ft_putchar_fd(c, 1);
+		c = 0;
+		kill(pd, SIGUSR1);
+	}
+	else
+		c <<= 1;
+}
+
+int	main(void)
+{
+	struct sigaction	act;
+
+	ft_putstr_fd("Server PID: ", STDOUT_FILENO);
+	ft_putnbr_fd(getpid(), STDOUT_FILENO);
+	ft_putchar_fd('\n', STDOUT_FILENO);
+	act.sa_sigaction = ft_rsignal;
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &act, 0);
+	sigaction(SIGUSR2, &act, 0);
+	while (1)
+		pause();
+	return (0);
+}
