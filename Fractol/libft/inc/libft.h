e38be1d70b9c6902e6d4673d2bbae868bdbb182e
PIPEFD <91981749+PIPEFD@users.noreply.github.com>
2024-03-05 22:00:59 +0000

    libft

---
+++ Fractol/libft/inc/libft.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 21:49:48 by codespace         #+#    #+#             */
+/*   Updated: 2024/03/05 21:49:49 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# define HEXX "0123456789abcdef"
+# define HEX "0123456789ABCDEF"
+
+# define MAX_INT 2147483647
+# define MIN_INT -2147483648
+
+# define RED "\033[0;31m"
+# define GREEN "\033[0;92m"
+
+
+
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <sys/types.h>
+# include <fcntl.h>
+
+
+
+
+#endif
